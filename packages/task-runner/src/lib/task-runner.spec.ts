316c9db61d323d267c104469a37371e3d07582db
minijus <3633549+minijus@users.noreply.github.com>
2022-02-05 20:13:43 +0200

    feat: add task runner

--- packages/task-runner/src/lib/task-runner.spec.ts
+++
@@ -1,7 +0,0 @@
-import { taskRunner } from './task-runner';
-
-describe('taskRunner', () => {
-  it('should work', () => {
-    expect(taskRunner()).toEqual('task-runner!');
-  });
-});
--- packages/task-runner/src/lib/task-runner.ts
+++ packages/task-runner/src/lib/task-runner.ts
@@ -1,3 +1,47 @@
-export function taskRunner(): string {
-  return 'task-runner!';
+import axios from 'axios';
+import FormData from 'form-data';
+import { createCustomRunner } from 'nx-remotecache-custom';
+import { Stream } from 'stream';
+
+// https://stackoverflow.com/questions/14269233/node-js-how-to-read-a-stream-into-a-buffer
+async function streamToBuffer(stream: Stream): Promise<Buffer> {
+  return new Promise<Buffer>((resolve, reject) => {
+    const buffer: Uint8Array[] = [];
+
+    stream.on('data', (chunk) => buffer.push(chunk));
+    stream.on('end', () => resolve(Buffer.concat(buffer)));
+    stream.on('error', (err) => reject(err));
+  });
 }
+
+export const taskRunner = createCustomRunner<{ url: string }>(
+  async (options) => {
+    return {
+      name: 'NXMN Task Runner',
+      fileExists: async (filename) => {
+        try {
+          const response = await axios.head(
+            `${options.url}/api/file/${filename}`
+          );
+          return response.status === 200;
+        } catch (e) {
+          return false;
+        }
+      },
+      retrieveFile: async (filename) => {
+        const response = await axios.get(
+          `${options.url}/api/file/${filename}`,
+          { responseType: 'stream' }
+        );
+        return streamToBuffer(response.data);
+      },
+      storeFile: async (filename, buffer) => {
+        const form = new FormData();
+        form.append('file', Buffer.from(buffer), filename);
+        return axios.post(`${options.url}/api/file`, form, {
+          headers: form.getHeaders(),
+        });
+      },
+    };
+  }
+);
