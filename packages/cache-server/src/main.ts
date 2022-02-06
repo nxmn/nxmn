93f94e9bbb68d5b3cb28e155d30588f28be51d91
minijus <3633549+minijus@users.noreply.github.com>
2022-02-06 17:03:41 +0200

    feat: setup TLS support (self signed certs included)

--- packages/cache-server/src/main.ts
+++ packages/cache-server/src/main.ts
@@ -3,10 +3,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let app;
+  let protocol = 'http';
+
+  if (process.env.PRIVATE_KEY && process.env.PUBLIC_CERTIFICATE) {
+    app = await NestFactory.create(AppModule, {
+      httpsOptions: {
+        key: process.env.PRIVATE_KEY,
+        cert: process.env.PUBLIC_CERTIFICATE,
+      },
+    });
+    protocol = 'https';
+  } else {
+    app = await NestFactory.create(AppModule);
+  }
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
+
+  Logger.log(`🚀 Application is running on: ${protocol}://localhost:${port}`);
 }
 
 bootstrap();
--- packages/task-runner/src/lib/task-runner.ts
+++ packages/task-runner/src/lib/task-runner.ts
@@ -1,5 +1,7 @@
-import axios from 'axios';
+import { Axios } from 'axios';
 import * as FormData from 'form-data';
+import { readFileSync } from 'fs';
+import { Agent } from 'https';
 import { createCustomRunner } from 'nx-remotecache-custom';
 import { Stream } from 'stream';
 
@@ -14,20 +16,27 @@ async function streamToBuffer(stream: Stream): Promise<Buffer> {
   });
 }
 
-export const taskRunner = createCustomRunner<{ url: string }>(
+export const taskRunner = createCustomRunner<{ url: string; caFile?: string }>(
   async (options) => {
+    const axios = new Axios({
+      baseURL: options.url,
+      httpsAgent: options.caFile
+        ? new Agent({ ca: readFileSync(options.caFile) })
+        : undefined,
+    });
+
     return {
       name: 'NXMN Task Runner',
       fileExists: async (filename) => {
         try {
-          const response = await axios.head(`${options.url}/cache/${filename}`);
+          const response = await axios.head(`/cache/${filename}`);
           return response.status === 200;
         } catch (e) {
           return false;
         }
       },
       retrieveFile: async (filename) => {
-        const response = await axios.get(`${options.url}/cache/${filename}`, {
+        const response = await axios.get(`/cache/${filename}`, {
           responseType: 'stream',
         });
         return streamToBuffer(response.data);
@@ -35,7 +44,7 @@ export const taskRunner = createCustomRunner<{ url: string }>(
       storeFile: async (filename, buffer) => {
         const form = new FormData();
         form.append('cache', Buffer.from(buffer), filename);
-        return axios.post(`${options.url}/cache`, form, {
+        return axios.post(`/cache`, form, {
           headers: form.getHeaders(),
         });
       },
